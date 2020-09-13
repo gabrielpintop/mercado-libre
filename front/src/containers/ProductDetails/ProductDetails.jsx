71dae6ccb4d6c0ebf87df3ecb2fbc1592d893fe6
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2020-09-13 17:33:55 -0500

    Product details is ready

--- front/src/containers/ProductDetails/ProductDetails.jsx
+++ front/src/containers/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { getProductById } from '../../services/products';
 import Loading from '../../components/Loading/Loading';
 import ProductsMessage from '../../components/ProductsMessage/ProductsMessage';
+import { formatNumber } from '../../services/utilities';
+import './ProductDetails.scss';
 
 const ProductDetails = ({ handleChanges }) => {
     const { id } = useParams();
@@ -12,17 +14,46 @@ const ProductDetails = ({ handleChanges }) => {
     useEffect(() => {
         handleChanges([], true);
         getProductById(id).then(data => {
-            // setProduct(data);
-            handleChanges([], false);
+            setProduct(data.item);
+            handleChanges(data.categories, false);
             setLoading(false);
         }).catch(err => {
-            window.alert(err);
             handleChanges([], false);
             setLoading(false);
         });
-    }, []);
+    }, [handleChanges, id]);
 
-    return (loading ? <Loading /> : product ? <h1>Hola</h1> : <ProductsMessage message="productNotFound" />);
+    const showPrice = () => {
+        if (product.price.decimals !== 0) {
+            const decimals = String(product.price.amount).substring(String(product.price.amount).length - product.price.decimals);
+            return <p id="productDetailsPrice" className="mb-medium"><span>$ {formatNumber(product.price.amount)}</span><sup>{decimals}</sup></p>;
+        } else {
+            return <p id="productDetailsPrice" className="mb-medium"><span>$ {formatNumber(product.price.amount)}</span><sup>00</sup></p>;
+        }
+    };
+
+    return (
+        loading ? <Loading /> : product ?
+            <div id="productDetails" className="main-content row no-gutters mb-medium pt-medium">
+                <div className="col-md-8">
+                    <div id="productDetailsImageContainer">
+                        <img src={product.picture} alt={product.title} />
+                    </div>
+                </div>
+                <div id="productDetailsInformation" className="col-md-4">
+                    <p className="mb-small">{product.condition} - {product.sold_quantity} vendidos</p>
+                    <h1 className="mb-medium">{product.title}</h1>
+                    {showPrice()}
+                    <a href={product.permalink} target="_blank" rel="noopener noreferrer">Comprar</a>
+                </div>
+                <div id="productsDetailsSeparator" className="col-12 mt-medium mb-medium"></div>
+                <div id="productDetailsDescription" className="col-md-8">
+                    <h3 className="mb-medium">Descripción del producto</h3>
+                    <p className="mb-0">{product.description}</p>
+                </div>
+            </div>
+            : <ProductsMessage message="productNotFound" />
+    );
 };
 
 export default ProductDetails;
--- front/src/services/products.js
+++ front/src/services/products.js
@@ -57,7 +57,7 @@ const getProducts = (q) => {
                         },
                         "picture": 'http://http2.mlstatic.com/D_949085-MLA40175707456_122019-F.jpg',
                         "condition": 'new',
-                        "free_shipping": Boolean
+                        "free_shipping": true
                     }]
             });
         }, 200);
@@ -73,19 +73,21 @@ const getProductById = (id) => {
                     "name": String,
                     "lastname": String,
                 },
+                "categories": ['Consolas', 'Consolas y Videojuegos'],
                 "item": {
                     "id": 'MLA863294524',
                     "title": 'Microsoft Xbox One S 1tb Two-controller Bundle Blanco',
                     "price": {
                         "currency": 'ARS',
-                        "amount": 55200,
-                        "decimals": 0
+                        "amount": 4332.22,
+                        "decimals": 2
                     },
                     "picture": 'http://http2.mlstatic.com/D_949085-MLA40175707456_122019-F.jpg',
-                    "condition": 'new',
-                    "free_shipping": Boolean,
-                    "sold_quantity": Number,
-                    "description": String
+                    "condition": "Nuevo",
+                    "free_shipping": true,
+                    "sold_quantity": 220,
+                    "description": 'asdasdasdasdasdasdas asdasfasfasf as fas d asd asdasdas fas d asd as d asd asdasdasdas as d asd as das d as',
+                    "permalink": "https://articulo.mercadolibre.com.ar/MLA-857699593-ipod-touch-4-gen-16-gb-impecable-_JM"
                 }
             });
         }, 200);
