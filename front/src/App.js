e88214ddac4296c8bf5196f2db1bd858264f5def
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2020-09-12 23:50:11 -0500

    Fix in button styles

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 
+const mockedLoadProducts = (result) => {
+  console.log(result);
+};
+
 const App = () => {
   return (
     <>
-      <SearchBar></SearchBar>
+      <SearchBar loadProducts={mockedLoadProducts}></SearchBar>
     </>
   );
 }
