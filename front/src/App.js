bd8722cce5bcea38df0684572215ce49ec3bc224
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2020-09-13 02:21:46 -0500

    Not used imports are removed

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import SearchBar from './components/SearchBar/SearchBar';
-import Breadcrumb from './components/Breadcrumb/Breadcrumb';
-import full from '../src/assets/02_Detalle@2x.png.png';
 import Products from './containers/Products/Products';
 import ProductDetails from './containers/ProductDetails/ProductDetails';
-
-const categories = ['Electrónica, Audio y Video', 'iPod', 'Reproductores', 'iPod touch', '32 GB'];
-const mockedLoadProducts = (result) => {
-  console.log(result);
-};
 
 const App = () => {
   return (
