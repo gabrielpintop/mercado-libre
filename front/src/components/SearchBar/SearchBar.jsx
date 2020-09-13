179399e7a792a561017e258a0cbc8039a1a292d3
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2020-09-12 22:33:02 -0500

    Search bar text input is handled

--- front/src/components/SearchBar/SearchBar.jsx
+++ front/src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.scss';
 import mercadoLibreLogo from '../../assets/logos/Logo_ML@2x.png';
 import searchIcon from '../../assets/icons/ic_Search@2x.png';
 
 const SearchBar = (props) => {
+    const [searchedText, setSearchedText] = useState('');
+    const defineText = (event) => {
+        setSearchedText(event.target.value);
+    };
+
     return (
         <header className="header">
             <nav className="row h-100 margin-horizontal">
@@ -13,7 +18,7 @@ const SearchBar = (props) => {
                     </div>
                     <div className="col-11 pl-0 pr-0">
                         <div id="searchBar">
-                            <input id="searchBarInput" type="text" placeholder="Nunca dejes de buscar" />
+                            <input id="searchBarInput" name="searchedText" type="text" placeholder="Nunca dejes de buscar" value={searchedText} onChange={defineText} />
                             <button style={{ backgroundImage: `url(${searchIcon})` }}></button>
                         </div>
                     </div>
--- front/src/components/SearchBar/SearchBar.test.js
+++ front/src/components/SearchBar/SearchBar.test.js
@@ -21,4 +21,12 @@ describe('component - SearchBar', () => {
         expect(searchBar.exists('#searchBarInput')).toBeTruthy();
         expect(searchBar.exists('#searchBar button')).toBeTruthy();
     });
+
+    test('input value change', () => {
+        const input = searchBar.find('#searchBarInput');
+        const searchedText = 'Testing';
+        expect(input.props().value).toEqual('');
+        input.simulate('change', { target: { value: 'Testing' } });
+        expect(searchBar.find('#searchBarInput').props().value).toEqual(searchedText);
+    });
 });
