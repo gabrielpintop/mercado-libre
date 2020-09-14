864d39af9fdf26c63a4077860d0e51c7e7b9e86b
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2020-09-13 22:18:13 -0500

    Final implementations of product details

--- back/components/item/itemService.js
+++ back/components/item/itemService.js
@@ -1,5 +1,51 @@
 const { getRequest } = require('../../utilities/httpsHandler');
 
+
+const extractProductData = ({ id, title, price, sold_quantity, currency_id, thumbnail, attributes, shipping, pictures, secure_thumbnail, permalink, seller_address }, full = false) => {
+    const stringPrice = String(price)
+    let decimals = 0;
+    if (stringPrice.indexOf('.') !== -1) {
+        decimals = stringPrice.split('.')[1].length;
+    }
+
+    let condition = '';
+    if (attributes) {
+        const conditionAttribute = attributes.find(attribute => attribute.id === 'ITEM_CONDITION');
+        if (conditionAttribute && conditionAttribute.values && conditionAttribute.values.length > 0) {
+            condition = conditionAttribute.values[0].name;
+        }
+    }
+
+    let picture = thumbnail;
+    let fallbackPicture = '';
+    const imageIndex = picture.indexOf('I.jpg')
+    if (imageIndex !== -1) {
+        fallbackPicture = picture;
+        picture = picture.substring(0, imageIndex) + 'E.jpg';
+    }
+
+    let product = { id, title, price: { amount: price, currency: currency_id, decimals, }, picture, fallbackPicture, condition, free_shipping: shipping ? shipping.free_shipping : false };
+
+    if (seller_address && seller_address.city) {
+        Object.assign(product, { city: seller_address.city.name });
+    }
+
+    if (full) {
+        const newPictures = pictures.map(pic => (pic.secure_url));
+        Object.assign(product, { sold_quantity: sold_quantity, permalink, pictures: newPictures });
+        if (newPictures.length > 0) {
+            product.picture = newPictures[0];
+        }
+    }
+
+    return product;
+};
+
+const extractCategories = (attributes = []) => {
+    const attributeValues = attributes.filter(attribute => attribute.id === 'BRAND' || attribute.id === 'LINE' || attribute.id === 'MODEL' || attribute.id === 'INTERNAL_MEMORY' || attribute.id === 'OPERATING_SYSTEM_NAME');
+    return attributeValues.map(attributeValue => attributeValue.value_name);
+};
+
 const getItems = async ({ q, offset = 8 }) => {
     try {
         let offsetQuery = '';
@@ -41,54 +87,38 @@ const getItems = async ({ q, offset = 8 }) => {
                     });
                 }
             }
-            // if (result.available_filters) {
-            //     categoryFilter = result.available_filters.find(filter => filter.id === 'category');
-            //     if (categoryFilter && categoryFilter.values) {
-            //         categoryFilter.values.forEach(value => {
-            //             currentFilters.add(value.name);
-            //         });
-            //     }
-            // }
+
             currentFilters.forEach(value => {
                 response.categories.push(value);
             });
 
             // Items information extraction
-            response.items = result.results.map(({ id, title, price, currency_id, thumbnail, attributes, shipping }) => {
-                const stringPrice = String(price)
-                let decimals = 0;
-                if (stringPrice.indexOf('.') !== -1) {
-                    decimals = stringPrice.split('.')[1].length;
-                }
-
-                let condition = '';
-                if (attributes) {
-                    const conditionAttribute = attributes.find(attribute => attribute.id === 'ITEM_CONDITION');
-                    if (conditionAttribute && conditionAttribute.values && conditionAttribute.values.length > 0) {
-                        condition = conditionAttribute.values[0].name;
-                    }
-                }
-
-                let picture = thumbnail;
-                let fallbackPicture = '';
-                const imageIndex = thumbnail.indexOf('I.jpg')
-                if (imageIndex !== -1) {
-                    picture = thumbnail.substring(0, imageIndex) + 'E.jpg';
-                    fallbackPicture = thumbnail;
-                }
-
-                return { id, title, price: { amount: price, currency: currency_id, decimals, }, picture, fallbackPicture, condition, free_shipping: shipping ? shipping.free_shipping : false };
-            });
+            response.items = result.results.map((product) => extractProductData(product));
 
             return { data: response };
         }
     } catch (error) {
-        return { error: true, data: error, message: 'Se presentó un error haciendo la petición' }
+        return { error: true, data: error, message: 'Se presentó un error haciendo la petición' };
     }
 };
 
 const getItemById = async (id) => {
-    return { data: { id } };
+    try {
+        const results = await Promise.all([getRequest(`items/${id}`), getRequest(`items/${id}/description`)]);
+        if (results[0] !== null && results[1] !== null) {
+            if (results[0].error === 'resource not found' || results[1].error === 'not_found') {
+                throw new Error('No se encontró el elemento con el identificador ingresado');
+            }
+            const product = extractProductData({ ...results[0] }, true);
+            Object.assign(product, { description: results[1].plain_text });
+            return { data: { item: product, categories: extractCategories(results[0].attributes), result: results[0] } };
+        }
+
+        throw new Error('Se presentó un error haciendo la petición');
+    } catch (error) {
+        console.log(error);
+        return { error: true, data: error, message: error.message ? error.message : 'Se presentó un error haciendo la petición' };
+    }
 };
 
 module.exports = { getItems, getItemById };
--- front/src/services/products.js
+++ front/src/services/products.js
@@ -9,30 +9,7 @@ const getProducts = (q, offset = 0) => {
 
 const getProductById = (id) => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve({
-                "author": {
-                    "name": String,
-                    "lastname": String,
-                },
-                "categories": ['Consolas', 'Consolas y Videojuegos'],
-                "item": {
-                    "id": 'MLA863294524',
-                    "title": 'Microsoft Xbox One S 1tb Two-controller Bundle Blanco',
-                    "price": {
-                        "currency": 'ARS',
-                        "amount": 4332.22,
-                        "decimals": 2
-                    },
-                    "picture": 'http://http2.mlstatic.com/D_949085-MLA40175707456_122019-F.jpg',
-                    "condition": "Nuevo",
-                    "free_shipping": true,
-                    "sold_quantity": 220,
-                    "description": 'asdasdasdasdasdasdas asdasfasfasf as fas d asd asdasdas fas d asd as d asd asdasdasdas as d asd as das d as',
-                    "permalink": "https://articulo.mercadolibre.com.ar/MLA-857699593-ipod-touch-4-gen-16-gb-impecable-_JM"
-                }
-            });
-        }, 200);
+        getRequest(`${api}/${id}`, resolve, reject, true);
     });
 };
 
