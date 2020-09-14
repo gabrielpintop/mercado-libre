26ee7abfa7a443d902a7f7cbf3cd0a15c0c58b58
Gabriel Leonardo Pinto Pineda <gl.pinto10@uniandes.edu.co>
2020-09-14 00:41:31 -0500

    Update Pagination.jsx

--- front/src/components/Pagination/Pagination.jsx
+++ front/src/components/Pagination/Pagination.jsx
@@ -4,15 +4,15 @@ import './Pagination.scss';
 const Pagination = ({ total, selectPage, offset, hasNext, hasBefore }) => {
     return (total > 4 &&
         <div id="paginationContainer" className="row margin-horizontal mt-medium mb-small">
-            <div className="col-md-2 col-5"></div>
-            <div id="backButtonContainer" className="col-md-3">
+            <div className="col-md-2 col-12"></div>
+            <div id="backButtonContainer" className="col-md-3 col-5">
                 {hasBefore && <button onClick={() => selectPage(offset - 4)}><span>&lsaquo;</span>&nbsp;Anterior</button>}
             </div>
             <div className="col-md-2 col-2"></div>
-            <div id="forwardButtonContainer" className="col-md-3">
+            <div id="forwardButtonContainer" className="col-md-3 col-5">
                 {hasNext && <button onClick={() => selectPage(offset + 4)}>Siguiente&nbsp;<span>&rsaquo;</span></button>}
             </div>
-            <div className="col-md-2 col-5"></div>
+            <div className="col-md-2 col-12"></div>
         </div>
     );
 };
